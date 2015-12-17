0b4d0b5ebea28d12cce4958542cd6e166be9c00d
jliao <jliao@linkedin.com>
2015-12-17 15:35:18 +0800

    Initial Commit

---
+++ LJURLRouterExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJURLRouterExample
+//
+//  Created by Jinxing Liao on 12/17/15.
+//  Copyright © 2015 Jinxing Liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJURLRouterExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LJURLRouterExample
+//
+//  Created by Jinxing Liao on 12/17/15.
+//  Copyright © 2015 Jinxing Liao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
